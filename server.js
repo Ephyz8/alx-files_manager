fb029e4902d00dc652f5d228992f29963a1f76ac
Ephyz8 <ziwoyaephraim@gmail.com>
2024-08-30 02:12:35 +0200

    File data

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import routes from './routes';
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+// Middleware to parse JSON
+app.use(express.json({ limit: '50mb' }));
+// Load all routes from routes/index.js
+app.use(routes);
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+export default app;
